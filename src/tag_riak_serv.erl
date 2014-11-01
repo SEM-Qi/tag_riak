fa0beb55538873cbd90da7af17b31d55d739b451
James Eagle <guseagja@student.gu.se>
2014-11-01 18:26:04 +0100

    Have updated server to test version of tag attck

--- src/tag_riak_serv.erl
+++ src/tag_riak_serv.erl
@@ -25,16 +25,71 @@ handle_call(update_taglist, _From, SocketPid) ->
      <<"thestrain">>,<<"thesuitelife">>,<<"thewaylifegoes">>,
      <<"thework">>,<<"theythinktheyre21">>,<<"theywack">>,
      <<"thingsToDoBeforeIDie">>,<<"thingstodobeforeidie">>,
-     <<"thisisasadmoment">>,<<"thisisntover">>]),
+     <<"thisisasadmoment">>,<<"thisisntover">>,<<"porn">>]),
 	% {ok, Taglist} = riakc_pb_socket:list_buckets(Pid),
 	{reply, Taglist, SocketPid};
 
 handle_call({gettag, Tag}, _From, SocketPid) ->
-	{ok, List} = riakc_pb_socket:list_keys(SocketPid, Tag),
-	NumTag = length(List),
-	Result = jiffy:encode({[{Tag, NumTag}]}),
-	% {ok, Taglist} = riakc_pb_socket:list_buckets(Pid),
-	{reply, Result, SocketPid}.
+  {Numtags1, Cotags1, TweetText1} = getTagBySec(1, Tag, SocketPID),
+  {Numtags2, Cotags2, TweetText2} = getTagBySec(2, Tag, SocketPID),
+  {Numtags3, Cotags3, TweetText3} = getTagBySec(3, Tag, SocketPID),
+  {Numtags4, Cotags4, TweetText4} = getTagBySec(4, Tag, SocketPID),
+  {Numtags5, Cotags5, TweetText5} = getTagBySec(5, Tag, SocketPID),
+  {Numtags6, Cotags6, TweetText6} = getTagBySec(6, Tag, SocketPID),
+  {Numtags7, Cotags7, TweetText7} = getTagBySec(7, Tag, SocketPID),
+  {Numtags8, Cotags8, TweetText8} = getTagBySec(8, Tag, SocketPID),
+  {Numtags9, Cotags9, TweetText9} = getTagBySec(9, Tag, SocketPID),
+  {Numtags10, Cotags10, TweetText10} = getTagBySec(10, Tag, SocketPID),
+  AllCotags = Cotags10 ++ Cotags1 ++ Cotags2 ++ 
+              Cotags3 ++ Cotags4 ++ Cotags5 ++ 
+              Cotags6 ++ Cotags7 ++ Cotags8 ++ Cotags9,
+
+Response = jiffy:encode({[{<<"tag">>, Tag},
+  {<<"cotags">>, AllCotags},
+  {<<"distribution">>, 
+    [{[{<<"numtags">>, Numtags1}, {<<"tweets">>, TweetText1}]},
+        {[{<<"numtags">>, Numtags2}, {<<"tweets">>, TweetText2}]},
+        {[{<<"numtags">>, Numtags3}, {<<"tweets">>, TweetText3}]},
+        {[{<<"numtags">>, Numtags4}, {<<"tweets">>, TweetText4}]},
+        {[{<<"numtags">>, Numtags5}, {<<"tweets">>, TweetText5}]},
+        {[{<<"numtags">>, Numtags6}, {<<"tweets">>, TweetText6}]},
+        {[{<<"numtags">>, Numtags7}, {<<"tweets">>, TweetText7}]},
+        {[{<<"numtags">>, Numtags8}, {<<"tweets">>, TweetText8}]},
+        {[{<<"numtags">>, Numtags9}, {<<"tweets">>, TweetText9}]},
+        {[{<<"numtags">>, Numtags10}, {<<"tweets">>, TweetText10}]}]}]}),
+
+{reply, Response, SocketPid}.
+
+% {[{foo, bar}]}
+
+% {[{<<"tag">>, <<"tag">>},
+%   {<<"cotags">>, [<<"cotags">>, <<"cotags">>]},
+%   {<<"distribution">>, 
+%     [{[{<<"numtags">>, 1}, {<<"tweets">>, [<<"tweets1">>, <<"tweets2">>]}]},
+%         {[{<<"numtags">>, 2}, {<<"tweets">>, [<<"tweets1">>, <<"tweets2">>]}]},
+%         {[{<<"numtags">>, 3}, {<<"tweets">>, [<<"tweets1">>, <<"tweets2">>]}]},
+%         {[{<<"numtags">>, 4}, {<<"tweets">>, [<<"tweets1">>, <<"tweets2">>]}]},
+%         {[{<<"numtags">>, 5}, {<<"tweets">>, [<<"tweets1">>, <<"tweets2">>]}]},
+%         {[{<<"numtags">>, 6}, {<<"tweets">>, [<<"tweets1">>, <<"tweets2">>]}]},
+%         {[{<<"numtags">>, 7}, {<<"tweets">>, [<<"tweets1">>, <<"tweets2">>]}]},
+%         {[{<<"numtags">>, 8}, {<<"tweets">>, [<<"tweets1">>, <<"tweets2">>]}]},
+%         {[{<<"numtags">>, 9}, {<<"tweets">>, [<<"tweets1">>, <<"tweets2">>]}]},
+%         {[{<<"numtags">>, 10}, {<<"tweets">>, [<<"tweets1">>, <<"tweets2">>]}]}]}]},
+% {
+%     "tag": "string",
+%     "cotags":["string"],
+%     "distribution": [
+%         {
+%             "numtags":"integer",
+%             "tweets":["string"]
+%         }]
+% }
+
+% {ok, List} = riakc_pb_socket:list_keys(SocketPid, Tag),
+% NumTag = length(List),
+% Result = jiffy:encode({[{Tag, NumTag}]}),
+% % {ok, Taglist} = riakc_pb_socket:list_buckets(Pid),
+% {reply, Result, SocketPid}.
 
 handle_cast(terminate, State) ->
   {noreply, State}.
@@ -49,3 +104,21 @@ terminate(_Reason, _State) ->
     ok.
 
 %Call - Start a server under tag_riak_sup supervision
+
+testoldTimeStamp(Second) ->
+  1414506936992434 - ((Second * 120) * 1000000).
+oldTimeStamp(Second) ->
+  {Mega, Secs, Micro} = erlang:now(),
+  Mega*1000*1000*1000*1000 + ((Secs - (Second *120)) * 1000 * 1000) + Micro.
+
+getTagBySec(Second, Tag, SocketPID) ->
+  {ok, {_,Keys,_,_}} = riakc_pb_socket:get_index_range(
+          SocketPID,
+          Tag, %% bucket name
+          {integer_index, "timestamp"}, %% index name
+          testoldTimeStamp(Second), testoldTimeStamp(Second-1) %% origin timestamp should eventually have some logic attached
+        ),
+  TagObjs = [fun() -> {ok, Obj} = riakc_pb_socket:get(SocketPid, Tag, Key), Obj end  || Key <- Keys],
+  Allofthecotags = lists:foldl(fun(Object, AllCotags) -> {Cotags, _} = riakc_obj:get_value(Object), Cotags ++ AllCotags end, [], TagObjs),
+  {length(Keys), Allofthecotags, [<<"dummy tweet text">>]}.
+
