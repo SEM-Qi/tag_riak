ea174c51a9a8782f20fd55712d191ee8f6b6cd36
James Eagle <guseagja@student.gu.se>
2014-11-04 03:32:50 +0100

    new hopefully faster verson of riak fetch

--- src/tag_riak_serv.erl
+++ src/tag_riak_serv.erl
@@ -16,47 +16,26 @@ init([]) ->
 %DO Call/Cast Handling here
 
 handle_call(update_taglist, _From, SocketPid) ->
-	Taglist = jiffy:encode({[{<<"tags">>,[<<"theifs">>,<<"thekilling">>,<<"them">>,
-     <<"themasterstoryteller">>,<<"themselves">>,
-     <<"thenextgreatbaker">>,<<"theprofit">>,<<"therapydisco">>,
-     <<"theseStreets">>,<<"theshatteredmind">>,
-     <<"theshinsekai">>,<<"thesimplethingstour">>,<<"thesis">>,
-     <<"thestrain">>,<<"thesuitelife">>,<<"thewaylifegoes">>,
-     <<"thework">>,<<"theythinktheyre21">>,<<"theywack">>,
-     <<"thingsToDoBeforeIDie">>,<<"thingstodobeforeidie">>,
-     <<"thisisasadmoment">>,<<"thisisntover">>,<<"porn">>]}]}),
+	case riakc_pb_socket:get(SocketPid, <<"taglistbucket">>, <<"taglist">>) of
+        {ok, CurrentTaglist} ->     
+          Taglist = jiffy:encode({[{<<"tags">>, CurrentTaglist}]});
+        {error,_} ->
+          Taglist = jiffy:encode({[{<<"tags">>, <<"Bad List">>}]})
+  end,
 	% {ok, Taglist} = riakc_pb_socket:list_buckets(Pid),
 	{reply, Taglist, SocketPid};
 
 handle_call({gettag, Tag}, _From, SocketPid) ->
-  {Numtags1, Cotags1, TweetText1} = getTagBySec(1, Tag, SocketPid),
-  {Numtags2, Cotags2, TweetText2} = getTagBySec(2, Tag, SocketPid),
-  {Numtags3, Cotags3, TweetText3} = getTagBySec(3, Tag, SocketPid),
-  {Numtags4, Cotags4, TweetText4} = getTagBySec(4, Tag, SocketPid),
-  {Numtags5, Cotags5, TweetText5} = getTagBySec(5, Tag, SocketPid),
-  {Numtags6, Cotags6, TweetText6} = getTagBySec(6, Tag, SocketPid),
-  {Numtags7, Cotags7, TweetText7} = getTagBySec(7, Tag, SocketPid),
-  {Numtags8, Cotags8, TweetText8} = getTagBySec(8, Tag, SocketPid),
-  {Numtags9, Cotags9, TweetText9} = getTagBySec(9, Tag, SocketPid),
-  {Numtags10, Cotags10, TweetText10} = getTagBySec(10, Tag, SocketPid),
-  AllCotags = Cotags10 ++ Cotags1 ++ Cotags2 ++ 
-              Cotags3 ++ Cotags4 ++ Cotags5 ++ 
-              Cotags6 ++ Cotags7 ++ Cotags8 ++ Cotags9,
-
-Response = jiffy:encode({[{<<"tag">>, Tag},
-  {<<"cotags">>, AllCotags},
+  {ok, Keys} = riakc_pb_socket:list_keys(SocketPid, <<"tags">>),
+  AllKeys = lists:reverse(lists:sort(Keys)),
+  {NewKeys,_} = lists:split(20, AllKeys),
+  Objects = lists:map(fun(Key) -> {ok, Obj} = riakc_pb_socket:get(SocketPid, <<"tags">>, Key), Obj end, NewKeys),
+  Tagset = lists:map(fun(Object) -> Value = binary_to_term(riakc_obj:get_value(Object)), {ok, Tag} = dict:find(Value), Tag end, Objects),
+  {Distribution, Cotags} = loopThrough(Tagset, [], sets:new()),
+  Response = jiffy:encode({[{<<"tag">>, Tag},
+  {<<"cotags">>, Cotags},
   {<<"distribution">>, 
-    [{[{<<"numtags">>, Numtags1}, {<<"tweets">>, TweetText1}]},
-        {[{<<"numtags">>, Numtags2}, {<<"tweets">>, TweetText2}]},
-        {[{<<"numtags">>, Numtags3}, {<<"tweets">>, TweetText3}]},
-        {[{<<"numtags">>, Numtags4}, {<<"tweets">>, TweetText4}]},
-        {[{<<"numtags">>, Numtags5}, {<<"tweets">>, TweetText5}]},
-        {[{<<"numtags">>, Numtags6}, {<<"tweets">>, TweetText6}]},
-        {[{<<"numtags">>, Numtags7}, {<<"tweets">>, TweetText7}]},
-        {[{<<"numtags">>, Numtags8}, {<<"tweets">>, TweetText8}]},
-        {[{<<"numtags">>, Numtags9}, {<<"tweets">>, TweetText9}]},
-        {[{<<"numtags">>, Numtags10}, {<<"tweets">>, TweetText10}]}]}]}),
-
+    Distribution}]}),
 {reply, Response, SocketPid}.
 
 % {[{foo, bar}]}
@@ -103,28 +82,35 @@ terminate(_Reason, _State) ->
     ok.
 
 %% temporary timestamp function
-testoldTimeStamp(Second) ->
-  1414506936992434 - ((Second * 120) * 1000000).
+% testoldTimeStamp(Second) ->
+%   1414506936992434 - ((Second * 120) * 1000000).
 
-%% When miner is live, this is the correct timestamp function
+% %% When miner is live, this is the correct timestamp function
 
-% oldTimeStamp(Second) ->
-%   {Mega, Secs, Micro} = erlang:now(),
-%   Mega*1000*1000*1000*1000 + ((Secs - (Second *120)) * 1000 * 1000) + Micro.
+% % oldTimeStamp(Second) ->
+% %   {Mega, Secs, Micro} = erlang:now(),
+% %   Mega*1000*1000*1000*1000 + ((Secs - (Second *120)) * 1000 * 1000) + Micro.
 
-getTagBySec(Second, Tag, SocketPid) ->
-  case riakc_pb_socket:get_index_range(
-            SocketPid,
-            Tag, %% bucket name
-            {integer_index, "timestamp"}, %% index name
-            testoldTimeStamp(Second), testoldTimeStamp(Second-1) %% origin timestamp should eventually have some logic attached
-          ) of
-    {ok, {_,[],_,_}} ->
-      {0, [], []};
-    {ok, {_,Keys,_,_}} ->
-      TagObjs = lists:map(fun(Key) -> {ok, Obj} = riakc_pb_socket:get(SocketPid, Tag, Key), Obj end, Keys),
-      Allofthecotags = lists:foldl(fun(Object, AllCotags) -> Binary = riakc_obj:get_value(Object), 
-                        {Cotags, _} = binary_to_term(Binary), Cotags ++ AllCotags end, [], TagObjs),
-      {length(Keys), Allofthecotags, [<<"dummy tweet text">>]}
-  end.
+% getTagBySec(Second, Tag, SocketPid) ->
+%   case riakc_pb_socket:get_index_range(
+%             SocketPid,
+%             Tag, %% bucket name
+%             {integer_index, "timestamp"}, %% index name
+%             testoldTimeStamp(Second), testoldTimeStamp(Second-1) %% origin timestamp should eventually have some logic attached
+%           ) of
+%     {ok, {_,[],_,_}} ->
+%       {0, [], []};
+%     {ok, {_,Keys,_,_}} ->
+%       TagObjs = lists:map(fun(Key) -> {ok, Obj} = riakc_pb_socket:get(SocketPid, Tag, Key), Obj end, Keys),
+%       Allofthecotags = lists:foldl(fun(Object, AllCotags) -> Binary = riakc_obj:get_value(Object), 
+%                         {Cotags, _} = binary_to_term(Binary), Cotags ++ AllCotags end, [], TagObjs),
+%       {length(Keys), Allofthecotags, [<<"dummy tweet text">>]}
+%   end.
 
+loopThrough([], L, Cotags) -> {L, Cotags};
+loopThrough(Tagset, L, OldCotags) ->
+  {NewKeys,OldKeys} = lists:split(2, Tagset),
+  [{Num, Cotags, Tweets}, {Num2, Cotags2, Tweets2}] = NewKeys,
+  L2 = [{[{<<"numtags">>, Num + Num2}, {<<"tweets">>, sets:union([Tweets, Tweets2])}]}|L],
+  NewCotags = sets:union([Cotags, Cotags2, OldCotags]),
+  loopThrough(OldKeys, L2, NewCotags).
