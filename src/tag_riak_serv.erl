6a4bbbbc3279f52511d43739568f71be0ad7a45a
James Eagle <guseagja@student.gu.se>
2014-11-06 12:17:50 +0100

    handle a short data set if twitterminer needs restarting

--- src/tag_riak_serv.erl
+++ src/tag_riak_serv.erl
@@ -13,8 +13,6 @@ init([]) ->
   {ok, Pid} = riakc_pb_socket:start_link("greedo.skip.chalmers.se", 8087),
   {ok, Pid}.
 
-%DO Call/Cast Handling here
-
 handle_call(update_taglist, _From, SocketPid) ->
 	case riakc_pb_socket:get(SocketPid, <<"taglistbucket">>, <<"taglist">>) of
         {ok, CurrentTaglist} -> 
@@ -23,52 +21,35 @@ handle_call(update_taglist, _From, SocketPid) ->
         {error,_} ->
           Taglist = jiffy:encode({[{<<"tags">>, <<"Bad List">>}]})
   end,
-	% {ok, Taglist} = riakc_pb_socket:list_buckets(Pid),
 	{reply, Taglist, SocketPid};
 
 handle_call({gettag, Tag}, _From, SocketPid) ->
   {ok, Keys} = riakc_pb_socket:list_keys(SocketPid, <<"tags">>),
   AllKeys = lists:reverse(lists:sort(Keys)),
-  {NewKeys,_} = lists:split(20, AllKeys),
-  Objects = lists:map(fun(Key) -> {ok, Obj} = riakc_pb_socket:get(SocketPid, <<"tags">>, Key), Obj end, NewKeys),
-  Tagset = lists:map(fun(Object) -> Value = binary_to_term(riakc_obj:get_value(Object)), case dict:find(Tag, Value) of {ok, Tagged} -> Tagged; error -> {0, sets:new(),sets:new()} end end, Objects),
-  {Distribution, Cotags} = loopThrough(Tagset, [], sets:new()),
+  if
+    length(AllKeys) >= 20 ->
+      {NewKeys,_} = lists:split(20, AllKeys),
+      Objects = lists:map(fun(Key) -> {ok, Obj} = riakc_pb_socket:get(SocketPid, <<"tags">>, Key), Obj end, NewKeys),
+      Tagset = lists:map(fun(Object) -> Value = binary_to_term(riakc_obj:get_value(Object)), case dict:find(Tag, Value) of {ok, Tagged} -> Tagged; error -> {0, sets:new(),sets:new()} end end, Objects),
+      {Distribution, Cotags} = loopThrough(Tagset, [], sets:new());
+    (length(AllKeys) >= 2) and (length(AllKeys) rem 2 =:= 0) ->
+      Objects = lists:map(fun(Key) -> {ok, Obj} = riakc_pb_socket:get(SocketPid, <<"tags">>, Key), Obj end, AllKeys),
+      Tagset = lists:map(fun(Object) -> Value = binary_to_term(riakc_obj:get_value(Object)), case dict:find(Tag, Value) of {ok, Tagged} -> Tagged; error -> {0, sets:new(),sets:new()} end end, Objects),
+      {Distribution, Cotags} = loopThrough(Tagset, [], sets:new());
+    length(AllKeys) >= 2 ->
+      [_|NewKeys] = AllKeys,
+      Objects = lists:map(fun(Key) -> {ok, Obj} = riakc_pb_socket:get(SocketPid, <<"tags">>, Key), Obj end, NewKeys),
+      Tagset = lists:map(fun(Object) -> Value = binary_to_term(riakc_obj:get_value(Object)), case dict:find(Tag, Value) of {ok, Tagged} -> Tagged; error -> {0, sets:new(),sets:new()} end end, Objects),
+      {Distribution, Cotags} = loopThrough(Tagset, [], sets:new());
+    true ->
+      {Distribution, Cotags} = {[{[{<<"numtags">>, 0}, {<<"tweets">>, ""}]}],[]}
+  end,
   Response = jiffy:encode({[{<<"tag">>, Tag},
   {<<"cotags">>, Cotags},
   {<<"distribution">>, 
     Distribution}]}),
+
 {reply, Response, SocketPid}.
-
-% {[{foo, bar}]}
-
-% {[{<<"tag">>, <<"tag">>},
-%   {<<"cotags">>, [<<"cotags">>, <<"cotags">>]},
-%   {<<"distribution">>, 
-%     [{[{<<"numtags">>, 1}, {<<"tweets">>, [<<"tweets1">>, <<"tweets2">>]}]},
-%         {[{<<"numtags">>, 2}, {<<"tweets">>, [<<"tweets1">>, <<"tweets2">>]}]},
-%         {[{<<"numtags">>, 3}, {<<"tweets">>, [<<"tweets1">>, <<"tweets2">>]}]},
-%         {[{<<"numtags">>, 4}, {<<"tweets">>, [<<"tweets1">>, <<"tweets2">>]}]},
-%         {[{<<"numtags">>, 5}, {<<"tweets">>, [<<"tweets1">>, <<"tweets2">>]}]},
-%         {[{<<"numtags">>, 6}, {<<"tweets">>, [<<"tweets1">>, <<"tweets2">>]}]},
-%         {[{<<"numtags">>, 7}, {<<"tweets">>, [<<"tweets1">>, <<"tweets2">>]}]},
-%         {[{<<"numtags">>, 8}, {<<"tweets">>, [<<"tweets1">>, <<"tweets2">>]}]},
-%         {[{<<"numtags">>, 9}, {<<"tweets">>, [<<"tweets1">>, <<"tweets2">>]}]},
-%         {[{<<"numtags">>, 10}, {<<"tweets">>, [<<"tweets1">>, <<"tweets2">>]}]}]}]},
-% {
-%     "tag": "string",
-%     "cotags":["string"],
-%     "distribution": [
-%         {
-%             "numtags":"integer",
-%             "tweets":["string"]
-%         }]
-% }
-
-% {ok, List} = riakc_pb_socket:list_keys(SocketPid, Tag),
-% NumTag = length(List),
-% Result = jiffy:encode({[{Tag, NumTag}]}),
-% % {ok, Taglist} = riakc_pb_socket:list_buckets(Pid),
-% {reply, Result, SocketPid}.
 
 handle_cast(terminate, State) ->
   {noreply, State}.
@@ -82,32 +63,6 @@ code_change(_OldVsn, State, _Extra) ->
 terminate(_Reason, _State) ->
     ok.
 
-%% temporary timestamp function
-% testoldTimeStamp(Second) ->
-%   1414506936992434 - ((Second * 120) * 1000000).
-
-% %% When miner is live, this is the correct timestamp function
-
-% % oldTimeStamp(Second) ->
-% %   {Mega, Secs, Micro} = erlang:now(),
-% %   Mega*1000*1000*1000*1000 + ((Secs - (Second *120)) * 1000 * 1000) + Micro.
-
-% getTagBySec(Second, Tag, SocketPid) ->
-%   case riakc_pb_socket:get_index_range(
-%             SocketPid,
-%             Tag, %% bucket name
-%             {integer_index, "timestamp"}, %% index name
-%             testoldTimeStamp(Second), testoldTimeStamp(Second-1) %% origin timestamp should eventually have some logic attached
-%           ) of
-%     {ok, {_,[],_,_}} ->
-%       {0, [], []};
-%     {ok, {_,Keys,_,_}} ->
-%       TagObjs = lists:map(fun(Key) -> {ok, Obj} = riakc_pb_socket:get(SocketPid, Tag, Key), Obj end, Keys),
-%       Allofthecotags = lists:foldl(fun(Object, AllCotags) -> Binary = riakc_obj:get_value(Object), 
-%                         {Cotags, _} = binary_to_term(Binary), Cotags ++ AllCotags end, [], TagObjs),
-%       {length(Keys), Allofthecotags, [<<"dummy tweet text">>]}
-%   end.
-
 loopThrough([], L, Cotags) -> {L, sets:to_list(Cotags)};
 loopThrough(Tagset, L, OldCotags) ->
   {NewKeys,OldKeys} = lists:split(2, Tagset),
@@ -115,5 +70,3 @@ loopThrough(Tagset, L, OldCotags) ->
   L2 = [{[{<<"numtags">>, Num + Num2}, {<<"tweets">>, sets:to_list(sets:union([Tweets, Tweets2]))}]}|L],
   NewCotags = sets:union([Cotags, Cotags2, OldCotags]),
   loopThrough(OldKeys, L2, NewCotags).
-
-  % sets:to_list(sets:union([Tweets, Tweets2]))
