e5219486f5db559ab22fa8e2028ef0ad8e985d41
James Eagle <guseagja@student.gu.se>
2014-12-09 02:59:00 +0100

    Changed tag query to match crunched tags

--- src/tag_riak_serv.erl
+++ src/tag_riak_serv.erl
@@ -60,43 +60,17 @@ handle_call({testpost, TestInfo}, _From, SocketPid) ->
   {reply, Result, SocketPid};
 
 handle_call({gettag, Tag}, _From, SocketPid) ->
-  {Distribution, Cotags} = case riakc_pb_socket:get_index_range(
-            SocketPid,
-            <<"tags">>, %% bucket name
-            {integer_index, "timestamp"}, %% index name
-            oldTimeStamp(), timeStamp() %% origin timestamp should eventually have some logic attached
-          ) of
-    {ok, {_,Keys,_,_}} ->
-      AllKeys = lists:reverse(lists:sort(Keys)),
-      if
-        length(AllKeys) >= 20 ->
-          {NewKeys,_} = lists:split(20, AllKeys),
-          Objects = lists:map(fun(Key) -> {ok, Obj} = riakc_pb_socket:get(SocketPid, <<"tags">>, Key), Obj end, NewKeys),
-          Tagset = lists:map(fun(Object) -> Value = binary_to_term(riakc_obj:get_value(Object)), case dict:find(Tag, Value) of {ok, Tagged} -> Tagged; error -> {0, sets:new(),sets:new()} end end, Objects),
-          {Distribution1, Cotags1} = loopThrough(Tagset, [], sets:new()),
-          {Distribution1, Cotags1};
-        (length(AllKeys) >= 2) and (length(AllKeys) rem 2 =:= 0) ->
-          Objects = lists:map(fun(Key) -> {ok, Obj} = riakc_pb_socket:get(SocketPid, <<"tags">>, Key), Obj end, AllKeys),
-          Tagset = lists:map(fun(Object) -> Value = binary_to_term(riakc_obj:get_value(Object)), case dict:find(Tag, Value) of {ok, Tagged} -> Tagged; error -> {0, sets:new(),sets:new()} end end, Objects),
-          {Distribution1, Cotags1} = loopThrough(Tagset, [], sets:new()),
-          {Distribution1, Cotags1};
-        length(AllKeys) >= 2 ->
-          [_|NewKeys] = AllKeys,
-          Objects = lists:map(fun(Key) -> {ok, Obj} = riakc_pb_socket:get(SocketPid, <<"tags">>, Key), Obj end, NewKeys),
-          Tagset = lists:map(fun(Object) -> Value = binary_to_term(riakc_obj:get_value(Object)), case dict:find(Tag, Value) of {ok, Tagged} -> Tagged; error -> {0, sets:new(),sets:new()} end end, Objects),
-          {Distribution1, Cotags1} = loopThrough(Tagset, [], sets:new()),
-          {Distribution1, Cotags1};
-        true ->
-          {[{[{<<"numtags">>, 0}, {<<"tweets">>, ""}]}],[]}
-      end;
-    {error, _} ->
-      {[{[{<<"numtags">>, 0}, {<<"tweets">>, ""}]}],[]}
+  Response = case riakc_pb_socket:get(SocketPid, <<"formattedtweets">>, Tag) of
+        {ok, TagAttack} -> 
+          FinalTagAttack = binary_to_term(riakc_obj:get_value(TagAttack)),  
+          FinalTagAttack;
+        {error,_} ->
+          {Distribution, Cotags} = {[{[{<<"numtags">>, 0}, {<<"tweets">>, ""}]}],[]}
+          Response1 = jiffy:encode({[{<<"tag">>, Tag}, 
+            {<<"cotags">>, Cotags},
+            {<<"distribution">>, Distribution}]}),
+          Response1
   end,
-  Response = jiffy:encode({[{<<"tag">>, Tag},
-  {<<"cotags">>, Cotags},
-  {<<"distribution">>, 
-    Distribution}]}),
-
 {reply, Response, SocketPid}.
 
 handle_cast(terminate, State) ->
@@ -115,21 +89,6 @@ terminate(_Reason, _State) ->
 %% Internal Function Definitions
 %% ------------------------------------------------------------------
 
-loopThrough([], L, Cotags) -> {L, sets:to_list(Cotags)};
-loopThrough(Tagset, L, OldCotags) ->
-  {NewKeys,OldKeys} = lists:split(2, Tagset),
-  [{Num, Cotags, Tweets}, {Num2, Cotags2, Tweets2}] = NewKeys,
-  L2 = [{[{<<"numtags">>, Num + Num2}, {<<"tweets">>, sets:to_list(sets:union([Tweets, Tweets2]))}]}|L],
-  NewCotags = sets:union([Cotags, Cotags2, OldCotags]),
-  loopThrough(OldKeys, L2, NewCotags).
-
-timeStamp() ->
-  {Mega, Secs, Micro} = erlang:now(),
-  Mega*1000*1000*1000*1000 + Secs * 1000 * 1000 + Micro.
-
-oldTimeStamp() ->
-  {Mega, Secs, Micro} = erlang:now(),
-  Mega*1000*1000*1000*1000 + ((Secs - 2400) * 1000 * 1000) + Micro.
 
 extract(K, L) ->
   case lists:keyfind(K, 1, L) of
