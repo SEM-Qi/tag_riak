769467ec7c2430797f2068b7a17494e71395245b
James Eagle <guseagja@student.gu.se>
2014-12-19 23:27:07 +0100

    Comments added, hand-in version.

--- src/tag_riak_serv.erl
+++ src/tag_riak_serv.erl
@@ -75,13 +75,21 @@ handle_call({gettag, Tag}, _From, {SocketPid, Rest}) ->
 %% Handles Authorization queries, returns true or false depending on key match
 
 handle_call({authorize, Data}, _From, {SocketPid, Rest}) ->
+  
+  %% Query consists of a string which requires to be parsed
   {found, AuthKey} = extract(<<"auth_key">>, Data),
   {found, UserId} = extract(<<"user_id">>, Data),
+
+  %% Gets current information from DB about the user 
   Result = riakc_pb_socket:get(SocketPid, <<"users">>, UserId),
   {ok, Object} = Result,
   RetrievedValues = riakc_obj:get_value(Object),
+
+  %% Extracts key from the users information from DB
   {_, Key} = lists:keyfind(auth_key, 1, binary_to_term(RetrievedValues)),
   NewAuthKey = binary_to_list(AuthKey),
+
+  %% Compares if a key that was sent in query and the one in DB match
   if NewAuthKey =:= Key
     ->
     {reply, "true", {SocketPid, Rest}};
@@ -92,18 +100,26 @@ handle_call({authorize, Data}, _From, {SocketPid, Rest}) ->
 %% Handles user information queries
 
 handle_call({getuserinfo, RawData}, _From, {SocketPid, Rest}) ->
+
+  %% Query consists of a string which requires to be parsed
   {Data} = jiffy:decode(RawData),
   {found, RawUserId} = extract(<<"user_id">>, Data),
   UserId = list_to_integer(binary_to_list(RawUserId)),
+
+  %% Checks if UserID exists in the sent query
   if UserId =:= not_found
     ->
     {reply, bad_request, {SocketPid, Rest}};
     true    ->
+
+      %% If it exists, checks if UserID already exists in DB
       Result = riakc_pb_socket:get(SocketPid, <<"users">>, integer_to_binary(UserId)),
       if Result =:= {error, notfound}
         ->
         {reply, bad_request, {SocketPid, Rest}};
         true ->
+
+          %% If everything went fine, Retrieves values accordingly belonged to userID
           {ok, Object} = Result,
           RetrievedValues = binary_to_term(riakc_obj:get_value(Object)),
           {reply, jiffy:encode({RetrievedValues}), {SocketPid, Rest}}
@@ -114,17 +130,24 @@ handle_call({getuserinfo, RawData}, _From, {SocketPid, Rest}) ->
 
 
 handle_call({updatekey, Data}, _From, {SocketPid, Rest}) ->
+
+  %% Query consists of a string which requires to be parsed
   {found, NewAuthKey} = extract(<<"auth_key">>, Data),
   {found, UserIdInGame} = extract(<<"user_id">>, Data),
+
   %% Retrieve values from DB
   Result = riakc_pb_socket:get(SocketPid, <<"users">>, UserIdInGame),
   {ok, Object} = Result,
   RetrievedValues = binary_to_term(riakc_obj:get_value(Object)),
+
   %% Extract everything that is in there and then place new
   {found, ProfileImg} = extract(profile_image_url, RetrievedValues),
   {found, ScreenName} = extract(screen_name,       RetrievedValues),
   {found, LastTags}   = extract(last_tags,         RetrievedValues),
   {found, MostTags}   = extract(most_used,         RetrievedValues),
+
+  %% Create a new user object according to the userID sent in query by putting the new key in it and old other information
+  NewUser = [{auth_key, NewAuthKey}, {profile_image_url, ProfileImg},
   NewUser = [{auth_key, NewAuthKey}, {profile_image_url, ProfileImg},
     {screen_name, ScreenName}, {last_tags, LastTags}, {most_used, MostTags}],
   RiakObj = riakc_obj:new(<<"users">>, UserIdInGame, term_to_binary(NewUser)),
@@ -134,16 +157,24 @@ handle_call({updatekey, Data}, _From, {SocketPid, Rest}) ->
 %% Sets the Auth key The key is generated on twitter sign in.
 
 handle_call({setkey, Data}, _From, {SocketPid, Rest}) ->
+
+  %% Query consists of a string which requires to be parsed
   {found, AuthKey} = extract(<<"key">>, Data),
   {found, ProfileImg} = extract(<<"profile_image_url">>, Data),
   {found, ScreenName} = extract(<<"screen_name">>, Data),
   {found, UserId} = extract(<<"user_id">>, Data),
+
+  %% If UserID is not found, send bad_request
   if UserId =:= not_found
     ->
     {reply, bad_request, {SocketPid, Rest}};
+
+    %% If it was found, retrieve all information from it
     true    ->
       OldUserData = riakc_pb_socket:get(SocketPid, <<"users">>, UserId),
       {LastTags, MostUsed} = case OldUserData of
+
+      %% Check if User has already played a game
       {error, notfound}
         ->
           {[], []};
@@ -154,10 +185,14 @@ handle_call({setkey, Data}, _From, {SocketPid, Rest}) ->
           MostUsedTemp = extract(<<"most_used">>, Value),
           {LastTagsTemp, MostUsedTemp}
       end,
+
+      %% Create a new user object by putting all the information needed from above
       NewUser = [{auth_key, binary_to_list(AuthKey)}, {profile_image_url, binary_to_list(ProfileImg)},
         {screen_name, binary_to_list(ScreenName)}, {last_tags, LastTags}, {most_used, MostUsed}],
       RiakObj = riakc_obj:new(<<"users">>, UserId, term_to_binary(NewUser)),
       riakc_pb_socket:put(SocketPid, RiakObj),
+
+      %% Send back an auth key
       {reply, binary_to_list(AuthKey), {SocketPid, Rest}}
   end.
 
